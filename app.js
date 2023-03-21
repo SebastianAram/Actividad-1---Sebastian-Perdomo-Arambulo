9b9d55e49ed6d87a4e76d53e403e416ccd6396c8
SebastianAram <sebastian.perdomo@est.iudigital.edu.co>
2023-03-21 16:02:36 -0500

    :white_check_mark: primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const tipoEquipo = require('./routes/tipoEquipo')
+const estadoEquipo = require('./routes/estadoEquipo')
+const usuarioEquipo = require('./routes/usuarioEquipo')
+const marcaEquipo = require('./routes/marcaEquipo')
+const inventarioEquipo = require('./routes/inventarioEquipo')
+
+app.use('/api/tipoequipos', tipoEquipo)
+app.use('/api/estadoequipos', estadoEquipo)
+app.use('/api/usuarioequipos', usuarioEquipo)
+app.use('/api/marcaequipos', marcaEquipo)
+app.use('/api/inventarioequipos', inventarioEquipo)
+
+
+module.exports = app
